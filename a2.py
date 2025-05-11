42e62e175e53f3806413c7d9777370d2800a0682
dimslalom <dimasgisthaa@email.com>
2025-05-11 15:18:11 +1000

    first commit

---
+++ a2.py
@@ -0,0 +1,17 @@
+# DO NOT modify or add any import statements
+from support import *
+from display import HearthView
+
+# Name:
+# Student Number: 
+# Favorite Building: 
+# -----------------------------------------------------------------------------
+
+# Define your classes and functions here
+
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
